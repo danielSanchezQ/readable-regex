34d6419f451e8bf3821861be368c12fdcfe79540
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2022-08-08 18:08:51 +0200

    Fix back_reference error reporting

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -7,20 +7,23 @@ use syn::{parse, Expr, LitStr};
 #[proc_macro_error]
 #[proc_macro]
 pub fn back_reference(stream: TokenStream) -> TokenStream {
-    let input: Expr = parse(stream).unwrap();
-    if let Expr::Lit(n) = input {
+    if let Ok(Expr::Lit(n)) = parse::<Expr>(stream.clone()) {
         let n: usize = n.to_token_stream().to_string().parse().unwrap_or_default();
 
         if n == 0 {
-            emit_call_site_error!("`n` must be an integer bigger than 0");
+            emit_call_site_error!(
+                "`n` must be an integer bigger than 0, got {}",
+                stream.to_string()
+            );
         }
 
         let back_ref = syn::Lit::Str(LitStr::new(&format!(r"\{}", n), Span::call_site()));
         quote!(#back_ref).into()
     } else {
-        panic!(
-            "Expected usize, found {}",
-            input.to_token_stream().to_string()
+        emit_call_site_error!(
+            "`n` must be an integer bigger than 0, got {}",
+            stream.to_string()
         );
+        quote!().into()
     }
 }
