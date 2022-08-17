55b6a5f67f21f38d01581af1d4fe6f590458a076
Daniel Sanchez Quiros <sanchez.quiros.daniel@gmail.com>
2022-08-17 22:50:57 +0200

    Added features block

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
-//! ### Regex for human beings.
+//! ## Regex for human beings.
 //!
 //! Regex is useful. But, sincerely, since code it is more read than written regex could
 //! be more understandable in a verbose mode.
 //!
 //! `readable-regex` crate is a set of tools to build those regexes in a verbose way. Which aims
 //! to improve readability of code.
+//!
+//! ### Examples
+//!
+//!
+//! ### Features
+//! * `re` => Use [`regex`] crate backend.
+//! * `re-fancy` => Use [`fancy_regex`] crate backend and expands this crate functionality.
 
 pub mod builders;
 mod constants;
