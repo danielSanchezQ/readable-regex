baf9ab9ad35998315a2fe2fb55eace7329e369b4
danielsanchezq <daniel.sanchez@iohk.io>
2022-08-24 17:28:32 +0200

    Added url and email

--- src/presets/network.rs
+++ src/presets/network.rs
@@ -1,5 +1,5 @@
-use crate::ReadableRe::Raw;
-use crate::{chars, either, exactly, group, starts_and_ends_with, ReadableRe};
+use crate::ReadableRe::{Period, Raw};
+use crate::{chars, either, exactly, group, one_or_more, ranged, starts_and_ends_with, ReadableRe};
 use once_cell::sync::Lazy;
 
 const U8: Lazy<ReadableRe> = Lazy::new(|| {
@@ -27,11 +27,34 @@ pub const IPV6_ADDRESS: Lazy<ReadableRe> = Lazy::new(|| {
     )
 });
 
+/// URL, found [here]()
+pub const SIMPLE_URL: ReadableRe = Raw(
+    r"(https?://)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)",
+);
+
+/// Email as per [W3C HTML5 specification](https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address)
+pub const EMAIL: ReadableRe =
+    // we trust that specification, no need to build a monster here when we already have one
+    Raw(
+        r"^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+    );
+
+pub const SIMPLE_EMAIL: Lazy<ReadableRe> = Lazy::new(|| {
+    starts_and_ends_with(
+        group(one_or_more(chars(r"a-z0-9_\.\+-")))
+            + "@".into()
+            + group(one_or_more(chars(r"\da-z\.-")))
+            + Period
+            + group(ranged(2..6, chars(r"a-z\."))),
+    )
+});
+
 #[cfg(test)]
 mod tests {
-    use crate::presets::network::{IPV4_ADDRESS, U8};
+    use crate::presets::network::{
+        EMAIL, IPV4_ADDRESS, IPV6_ADDRESS, SIMPLE_EMAIL, SIMPLE_URL, U8,
+    };
     use crate::starts_and_ends_with;
-    use crate::ReadableRe::Raw;
 
     #[test]
     fn u8_re() {
@@ -108,9 +131,59 @@ mod tests {
             "2001:db8::1234:5678:5.6.7.8",
             "2001:db8:3:4:5::192.0.2.33",
         ];
-        let query = Raw(r"(?i)(?:[\da-f]{0,4}:){1,7}(?:(?P<ipv4>(?:(?:25[0-5]|2[0-4]\d|1?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|1?\d\d?))|[\da-f]{0,4})").compile().unwrap();
+        let query = IPV6_ADDRESS.compile().unwrap();
         for ip in ips {
             assert!(query.is_match(ip));
+        }
+    }
+
+    #[test]
+    fn url() {
+        let urls = [
+            "http://foo.com/blah_blah",
+            "http://foo.com/blah_blah/",
+            "http://foo.com/blah_blah_(wikipedia)",
+            "http://www.example.com/wpstyle/?p=364",
+            "https://www.example.com/foo/?bar=baz&inga=42&quux",
+            "http://userid:password@example.com:8080",
+            "http://foo.com/blah_(wikipedia)#cite-1",
+            "google.com",
+            "www.google.com",
+        ];
+        let query = SIMPLE_URL.compile().unwrap();
+        for url in urls {
+            assert!(query.is_match(url), "Failed to match: {}", url);
+        }
+    }
+
+    const EMAIL_CASES: &[&str] = &[
+        "email@example.com",
+        "firstname.lastname@example.com",
+        "email@subdomain.example.com",
+        "firstname+lastname@example.com",
+        "1234567890@example.com",
+        "email@example-one.com",
+        "_______@example.com",
+        "email@example.name",
+        "email@example.museum",
+        "email@example.co.jp",
+        "firstname-lastname@example.com",
+        "_@baz.com",
+    ];
+
+    #[test]
+    fn simple_email() {
+        let query = SIMPLE_EMAIL.compile().unwrap();
+        for mail in EMAIL_CASES {
+            assert!(query.is_match(mail), "Failed to match: {}", mail);
+        }
+    }
+
+    #[test]
+    fn email() {
+        let query = EMAIL.compile().unwrap();
+        for mail in EMAIL_CASES {
+            assert!(query.is_match(mail), "Failed to match: {}", mail);
         }
     }
 }
